d7cd05ad4b935494b3bb21497f170d2662e4f1d2
knilesh9 <neutron_stays@protonmail.com>
2024-04-04 17:30:48 +0530

    Fix: analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,11 @@ import store from './store';
 import TaskInput from './TaskInput';
 import TaskList from './TaskList';
 import { Analytics } from "@vercel/analytics/react"
-
 const App = () => {
   return (
     <Provider store={store}>
-      <Analytics/>
       <div className="min-h-screen flex flex-col justify-center items-center px-4 py-8 sm:px-6 lg:px-8">
+      <Analytics/>
         <div className="max-w-md w-full">
           <h1 className="text-3xl font-bold mb-4 text-center">To-Do App</h1>
           <TaskInput />
