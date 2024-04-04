a194c1e39f6dde4f57393088167a97256a13de43
knilesh9 <neutron_stays@protonmail.com>
2024-04-04 17:28:34 +0530

    Update: analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { Provider } from 'react-redux';
 import store from './store';
 import TaskInput from './TaskInput';
 import TaskList from './TaskList';
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
     <Provider store={store}>
+      <Analytics/>
       <div className="min-h-screen flex flex-col justify-center items-center px-4 py-8 sm:px-6 lg:px-8">
         <div className="max-w-md w-full">
           <h1 className="text-3xl font-bold mb-4 text-center">To-Do App</h1>
